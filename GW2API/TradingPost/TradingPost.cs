0b6769b149ad5370bc5527aef3f3927732e1345c
Ben Dunkin <chewygumball@gmail.com>
2013-09-01 00:28:54 -0400

    initial commit

---
+++ GW2API/TradingPost/TradingPost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW2API.TradingPost
+{
+    public static class TradingPost
+    {
+        private static string APIAddress = "https://tradingpost-live.ncplatform.net";
+    }
+}
